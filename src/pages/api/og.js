cf7b3aecdef436d5ee8cb5a359130c4897c283e8
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 14:41:34 +0530

    testing

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -1,75 +1,50 @@
-// pages/api/og.js
-
-import { ImageResponse } from "@vercel/og";
+import { ImageResponse } from '@vercel/og'
 
 export const config = {
-    runtime: "edge",
-};
+  runtime: 'edge',
+}
 
-export default function handler(req) {
-
-  // console.log('req', new URL(req.url))
-  //   const { searchParams } = new URL(req.url);
-  //   const DOMAIN = "https://meta-psi-five.vercel.app";
-  //   console.log("🚀 ~ handler ~ searchParams:", searchParams);
-  //   const hasTitle = searchParams?.has("title");
-  //   const hasImageUrl = searchParams?.has("amp;imageUrl");
-  //   const title = hasTitle ? searchParams?.get("title") : "New collection";
-
-  //   const imageUrl = hasImageUrl
-  //       ? searchParams?.get("amp;imageUrl")
-  //       : `${DOMAIN}/public/Sample-png-image-200kb.png`;
-
-    return new ImageResponse(
-        (
-            <div
-                style={{
-                    display: "flex",
-                    height: "100%",
-                    width: "100%",
-                    position: "relative",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    fontFamily: "Arial, sans-serif",
-                }}
-            >
-                <img
-                    src={'https://d17ha18jyelis7.cloudfront.net/collections/banners/0f62c3d4-d350-43fc-8f80-451cba0dd6a8-1707459809309'}
-                    alt="Background"
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                />
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        padding: "20px",
-                        borderRadius: "10px",
-                        color: "black",
-                        fontSize: "50px",
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        whiteSpace: "pre-wrap",
-                        zIndex: 1,
-                    }}
-                >
-                    {'title'}
-                </div>
-            </div>
-        ),
-        {
-          width: 1200,
-          height: 630,
-        }
-    );
+export default async function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: '#fff',
+          backgroundImage:
+            'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+          backgroundSize: '100px 100px',
+          height: '100%',
+          width: '100%',
+          textAlign: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          display: 'flex',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="Vercel"
+          width={255}
+          height={225}
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTE2IiBoZWlnaHQ9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGNsaXAtcnVsZT0iZXZlbm9kZCIgZD0iTTU3LjUgMEwxMTUgMTAwSDBMNTcuNSAweiIgLz48L3N2Zz4="
+          style={{ margin: '0 75px' }}
+        />
+        <div
+          style={{
+            fontSize: 60,
+            marginTop: 30,
+            lineHeight: 1.8,
+          }}
+        >
+          Vercel Edge Network
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  )
 }
