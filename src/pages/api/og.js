621e28cf7b26d283a64d41b3430507e4b2eedaa4
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 11:16:24 +0530

    testing

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -22,53 +22,53 @@ export default function handler(req) {
     return new ImageResponse(
         (
             <div
-                style={{
-                    display: "flex",
-                    height: "100%",
-                    width: "100%",
-                    position: "relative",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    fontFamily: "Arial, sans-serif",
-                }}
+                // style={{
+                //     display: "flex",
+                //     height: "100%",
+                //     width: "100%",
+                //     position: "relative",
+                //     alignItems: "center",
+                //     justifyContent: "center",
+                //     fontFamily: "Arial, sans-serif",
+                // }}
             >
                 <img
                     src={imageUrl}
                     alt="Background"
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
+                    // style={{
+                    //     position: "absolute",
+                    //     top: 0,
+                    //     left: 0,
+                    //     right: 0,
+                    //     bottom: 0,
+                    //     width: "100%",
+                    //     height: "100%",
+                    //     objectFit: "cover",
+                    // }}
                 />
                 <div
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        padding: "20px",
-                        borderRadius: "10px",
-                        color: "black",
-                        fontSize: "50px",
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        whiteSpace: "pre-wrap",
-                        zIndex: 1,
-                    }}
+                    // style={{
+                    //     position: "absolute",
+                    //     top: "50%",
+                    //     left: "50%",
+                    //     transform: "translate(-50%, -50%)",
+                    //     padding: "20px",
+                    //     borderRadius: "10px",
+                    //     color: "black",
+                    //     fontSize: "50px",
+                    //     fontWeight: "bold",
+                    //     textAlign: "center",
+                    //     whiteSpace: "pre-wrap",
+                    //     zIndex: 1,
+                    // }}
                 >
                     {title}
                 </div>
             </div>
         ),
-        {
-            width: 300,
-            height: 158,
-        }
+        // {
+        //     width: 300,
+        //     height: 158,
+        // }
     );
 }
