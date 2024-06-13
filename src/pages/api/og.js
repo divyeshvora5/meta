802e69c150b728e3f3950561c61007b33b4d9f48
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 11:31:43 +0530

    testing

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -1,72 +1,62 @@
-import { ImageResponse } from "@vercel/og";
+// pages/api/og.js
+
+import { ImageResponse } from '@vercel/og';
 
 export const config = {
-    runtime: "edge",
+  runtime: 'edge',
 };
 
 export default function handler(req) {
-    const { searchParams } = new URL(req.url);
-    const DOMAIN = "https://meta-psi-five.vercel.app";
-    console.log("🚀 ~ handler ~ searchParams:", searchParams);
-    const hasTitle = searchParams.has("title");
-    const hasImageUrl = searchParams.has("imageUrl");
-    const title = hasTitle ? searchParams.get("title") : "New collection";
+  const { searchParams } = new URL(req.url);
+  const title = searchParams.get('title') || 'Default Title';
+  const imageUrl = searchParams.get('imageUrl') || 'https://your-default-image-url.com/default-image.jpg';
 
-    const imageUrl = hasImageUrl
-        ? searchParams.get("imageUrl")
-        : `${DOMAIN}/public/Sample-png-image-200kb.png`;
-    
-    console.log("🚀 ~ handler ~ imageUrl:", imageUrl)
-    return new ImageResponse(
-        (
-            <div
-                style={{
-                    display: "flex",
-                    height: "100%",
-                    width: "100%",
-                    position: "relative",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    fontFamily: "Arial, sans-serif",
-                }}
-            >
-                <img
-                    src={imageUrl}
-                    alt="Background"
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                />
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        padding: "20px",
-                        borderRadius: "10px",
-                        color: "black",
-                        fontSize: "50px",
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        whiteSpace: "pre-wrap",
-                        zIndex: 1,
-                    }}
-                >
-                    {title}
-                </div>
-            </div>
-        ),
-        {
-            width: 1200,
-            height: 630,
-        }
-    );
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          position: 'relative',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontFamily: 'Arial, sans-serif',
+        }}
+      >
+        <img
+          src={imageUrl}
+          alt="Background"
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            padding: '20px',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Darker background for better contrast
+            borderRadius: '10px',
+            color: 'white',
+            fontSize: '50px',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {title}
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,130 +5,125 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const DOMAIN = "https://meta-psi-five.vercel.app"; // Replace with your domain
-const title = "BITMATIC ART";
-const description = "Your Page Description";
-const imgUrl = "https://hextoymedia.s3.us-east-1.amazonaws.com/users/lows/31b26cf4-f0fb-453a-b9c0-f5b214151782-1716539450001";
+const DOMAIN = 'https://meta-psi-five.vercel.app'; // Replace with your domain
+const title = 'BITMATIC ART';
+const description = 'Your Page Description';
+const imgUrl = 'https://d17ha18jyelis7.cloudfront.net/collections/originals/16395541-68bd-4f66-a581-6528fb9ffbf4-1705050528582';
 const ogImageUrl = `${DOMAIN}/api/og?title=${encodeURIComponent(title)}&imageUrl=${encodeURIComponent(imgUrl)}`;
 
-console.log("🚀 ~ ogImageUrl:", ogImageUrl);
-
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={description} />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={DOMAIN} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={ogImageUrl} />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={DOMAIN} />
-                <meta property="twitter:title" content={title} />
-                <meta property="twitter:description" content={description} />
-                <meta property="twitter:image" content={ogImageUrl} />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={`${styles.main} ${inter.className}`}>
-                <div className={styles.description}>
-                    <p>
-                        Get started by editing&nbsp;
-                        <code className={styles.code}>src/pages/index.js</code>
-                    </p>
-                    <div>
-                        <a
-                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            By{" "}
-                            <Image
-                                src="/vercel.svg"
-                                alt="Vercel Logo"
-                                className={styles.vercelLogo}
-                                width={100}
-                                height={24}
-                                priority
-                            />
-                        </a>
-                    </div>
-                </div>
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={DOMAIN} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={DOMAIN} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={ogImageUrl} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>src/pages/index.js</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
 
-                <div className={styles.center}>
-                    <Image
-                        className={styles.logo}
-                        src="/next.svg"
-                        alt="Next.js Logo"
-                        width={180}
-                        height={37}
-                        priority
-                    />
-                </div>
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
 
-                <div className={styles.grid}>
-                    <a
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Docs <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Find in-depth information about Next.js features
-                            and&nbsp;API.
-                        </p>
-                    </a>
+        <div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p>
+              Find in-depth information about Next.js features and&nbsp;API.
+            </p>
+          </a>
 
-                    <a
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Learn <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Learn about Next.js in an interactive course
-                            with&nbsp;quizzes!
-                        </p>
-                    </a>
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
 
-                    <a
-                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Templates <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Discover and deploy boilerplate example
-                            Next.js&nbsp;projects.
-                        </p>
-                    </a>
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p>
+              Discover and deploy boilerplate example Next.js&nbsp;projects.
+            </p>
+          </a>
 
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2>
-                            Deploy <span>-&gt;</span>
-                        </h2>
-                        <p>
-                            Instantly deploy your Next.js site to a shareable
-                            URL with&nbsp;Vercel.
-                        </p>
-                    </a>
-                </div>
-            </main>
-        </>
-    );
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p>
+              Instantly deploy your Next.js site to a shareable URL
+              with&nbsp;Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
