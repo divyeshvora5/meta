09f31143775bda575e8d730ae44e7bc74a8f7695
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 11:37:19 +0530

    testing

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -1,62 +1,73 @@
-// pages/api/og.js
-
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from "@vercel/og";
 
 export const config = {
-  runtime: 'edge',
+    runtime: "edge",
 };
 
 export default function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const title = searchParams.get('title') || 'Default Title';
-  const imageUrl = searchParams.get('imageUrl') || 'https://your-default-image-url.com/default-image.jpg';
+    const { searchParams } = new URL(req.url);
+    const DOMAIN = "https://meta-psi-five.vercel.app";
+    console.log("🚀 ~ handler ~ searchParams:", searchParams);
+    const hasTitle = searchParams.has("title");
+    const hasImageUrl = searchParams.has("imageUrl");
+    const title = hasTitle ? searchParams.get("title") : "New collection";
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: 'flex',
-          height: '100%',
-          width: '100%',
-          position: 'relative',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontFamily: 'Arial, sans-serif',
-        }}
-      >
-        <img
-          src={imageUrl}
-          alt="Background"
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-          }}
-        />
-        <div
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            padding: '20px',
-            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Darker background for better contrast
-            borderRadius: '10px',
-            color: 'white',
-            fontSize: '50px',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {title}
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+    const imageUrl = hasImageUrl
+        ? searchParams.get("imageUrl")
+        : `https://d17ha18jyelis7.cloudfront.net/collections/originals/16395541-68bd-4f66-a581-6528fb9ffbf4-1705050528582`;
+    
+    console.log("🚀 ~ handler ~ imageUrl:", imageUrl);
+    
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    display: "flex",
+                    height: "100%",
+                    width: "100%",
+                    position: "relative",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontFamily: "Arial, sans-serif",
+                }}
+            >
+                <img
+                    src={imageUrl}
+                    alt="Background"
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                    }}
+                />
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        padding: "20px",
+                        borderRadius: "10px",
+                        color: "black",
+                        fontSize: "50px",
+                        fontWeight: "bold",
+                        textAlign: "center",
+                        whiteSpace: "pre-wrap",
+                        zIndex: 1,
+                    }}
+                >
+                    {title}
+                </div>
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 630,
+        }
+    );
 }
