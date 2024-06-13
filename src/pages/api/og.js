4eef7e5f15cbdb5b73bdb237c797ac042d31428c
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 14:29:09 +0530

    testing

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -8,7 +8,7 @@ export const config = {
 
 export default function handler(req) {
 
-  console.log('req', req)
+  console.log('req', new URL(req.url))
     const { searchParams } = new URL(req.url);
     const DOMAIN = "https://meta-psi-five.vercel.app";
     console.log("🚀 ~ handler ~ searchParams:", searchParams);
