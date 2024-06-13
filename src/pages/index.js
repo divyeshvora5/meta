b44acd32eb987ffc0ebccdcad84dea41a4432b16
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-13 14:43:51 +0530

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,17 +20,18 @@ export default function Home() {
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={DOMAIN} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={DOMAIN} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={ogImageUrl} />
+        <meta name="og:title" content="Vercel Edge Network" />
+        <meta name="og:description" content="Vercel Edge Network" />
+        <meta
+          name="og:image"
+          content={
+            // Because OG images must have a absolute URL, we use the
+            // `VERCEL_URL` environment variable to get the deployment’s URL.
+            // More info:
+            // https://vercel.com/docs/concepts/projects/environment-variables
+            `${DOMAIN}/api/og`
+          }
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
